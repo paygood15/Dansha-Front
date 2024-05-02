6dc2d589a296038f79815bd9378d37693f80b79a
paygood15 <167597519+paygood15@users.noreply.github.com>
2024-05-02 12:02:35 +0300

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -81,7 +81,7 @@ useEffect(() => {
        <p className="text-pu text-purple-500 text-sm text-center sm:text-left">© 2020 Dansha —
          <a className="text-blue-500" href="https://twitter.com/knyttneve" rel="noopener noreferrer"  target="_blank">@build by webix</a>
        </p>
-       <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+       <span className="inline-flex sm:ml-auto sm:mt-0  justify-center sm:justify-start">
          <a className="text-gray-500">
            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
